0d0a0de19416001ec5496f076f8497f8838958a4
jakekeech <jakekeech@gmail.com>
2024-02-24 17:56:51 +0800

    fixed backend link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //   baseURL: "https://song-recommender-backend.onrender.com",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://song-recommender-backend.onrender.com",
 });
 
 export default axiosInstance;
