26fe5831d22c507c3d6823eda2ef41d155f6193f
jakekeech <jakekeech@gmail.com>
2024-03-08 20:47:19 +0800

    added long-loading text

--- client/src/components/LoadingFull.jsx
+++ client/src/components/LoadingFull.jsx
@@ -4,6 +4,7 @@ import BarLoader from "react-spinners/BarLoader";
 
 const LoadingFull = () => {
   const [screenWidth, setScreenWidth] = useState(100);
+  const [uptime, setUptime] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
@@ -17,12 +18,30 @@ const LoadingFull = () => {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setUptime((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="loading-main">
       <GiMusicalNotes className="w-20 h-20 mb-5 text-[#357960]" />
       <BarLoader color="#357960" width={screenWidth} />
+      {uptime > 5 ? (
+        <div className="md:max-w-[350px] max-w-[300px] mt-5">
+          <p className="text-center text-sm italic">
+            Our backend is initializing, which may take a moment. Thank you for
+            your patience!
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
